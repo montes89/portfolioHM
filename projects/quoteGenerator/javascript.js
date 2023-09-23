227e95467fa0a6c3203d1b4a234087ddff633d2b
Hector Montes <hmontes1289@gmail.com>
2023-09-23 11:16:24 -0500

    adding to project folder and mobile back ground photo

--- projects/quoteGenerator/javascript.js
+++ projects/quoteGenerator/javascript.js
@@ -2,16 +2,18 @@ var quotes = ['Life is what happens to us while we are making other plans.― Al
 'Life isn\’t about finding yourself. Life is about creating yourself.― George Bernard Shaw', 
 'Doubt kills more dreams than failure ever will. – Suzy Kassem', 
 'Keep your face always toward the sunshine, and shadows will fall behind you. – Walt Whitman',
- 'Whether you think you can or think you can\’t, you\’re right. – Henry Ford', 
- 'Your talent determines what you can do. Your motivation determines how much you\’re willing to do. Your attitude determines how well you do it. —Lou Holtz',
+'Whether you think you can or think you can\’t, you\’re right. – Henry Ford', 
+'Your talent determines what you can do. Your motivation determines how much you\’re willing to do. Your attitude determines how well you do it. —Lou Holtz',
 'The happiness of your life depends on the quality of your thoughts. – Marcus Aurelius',
 'You are who you are meant to be. Dance as if no one\’s watching. Love as if it\’s all you know. Dream as if you\’ll live forever. Live as if you\’ll die today. James Dean',
 'You do not find the happy life. You make it.— Camilla Eyring Kimball', 
 'It is during our darkest moments that we must focus to see the light. — Aristotle',
 'Develop success from failures. Discouragement and failure are two of the surest stepping stones to success. -Dale Carnegie',
-'Just because it’s what’s done doesn\’t mean it\’s what should be done!- Cinderella', 'It’s no use going back to yesterday because I was a different person then. – Alice, Alice In Wonderland',
+'Just because it’s what’s done doesn\’t mean it\’s what should be done!- Cinderella',
+'It’s no use going back to yesterday because I was a different person then. – Alice, Alice In Wonderland',
 'Learn as if you will live forever, live like you will die tomorrow. — Mahatma Gandhi',
-'All our dreams can come true if we have the courage to pursue them.- Walt Disney']
+'All our dreams can come true if we have the courage to pursue them.- Walt Disney',
+'Bruh!!! - Wendy Davila','All that glitters is not gold - William Shakespeare']
 
 
 function newQuote() {
